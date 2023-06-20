9ac889655ad9a182651d27559ec30d2c1c6bc5ab
Sean McDaniel <seanhb714@gmail.com>
2023-06-20 10:03:59 -0700

    RecipeGenerator Form Created

--- src/components/RecipeGenerator.jsx
+++ src/components/RecipeGenerator.jsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+
 const RecipeGenerator = () => {
+
+  const [recipe, setRecipe] = useState('')
+  const [calories, setCalories] = useState(0);
+  const [protein, setProtein] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert(`Generating recipe for a ${recipe} with ${calories} calories and ${protein}g of protein`)
+  }
+  
   return (
-    <h2>Generate Recipe</h2>
+    <div className="recipeGenerator">
+      <h2>What are you craving today?</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter Recipe:
+          <input
+            type="text"
+            value={recipe}
+            onChange={(e) => setRecipe(e.target.value)} />
+        </label>
+        <label>
+          Enter Calorie Limit:
+          <input 
+            type="text"
+            value={calories}
+            onChange={(e) => setCalories(e.target.value)} />
+        </label>
+        <label>
+          Enter Protein Goal:
+          <input 
+            type="text"
+            value={protein}
+            onChange={(e) => setProtein(e.target.value)} />
+        </label>
+        <input type="submit" />
+      </form>
+
+    </div>
   )
 }
 
