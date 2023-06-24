51819f7d64adc3867d8b51f7b4ead3f83e349b8f
Sean McDaniel <seanhb714@gmail.com>
2023-06-24 10:33:35 -0700

    open AI Imports

---
+++ server.js
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Configuration,OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
