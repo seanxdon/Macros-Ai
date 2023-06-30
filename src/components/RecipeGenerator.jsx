c16ce5556af9a30f1e3489be4204075d5bb57849
Sean McDaniel <seanhb714@gmail.com>
2023-06-29 23:14:35 -0700

    API response logged to console from Recipe input field

--- src/components/RecipeGenerator.jsx
+++ src/components/RecipeGenerator.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const RecipeGenerator = () => {
 
   const [recipe, setRecipe] = useState('')
   const [calories, setCalories] = useState();
   const [protein, setProtein] = useState();
+  const formEl = useRef(null);
+  const resultEl = useRef(null);
 
-  function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert(`Generating recipe for a ${recipe} with ${calories} calories and ${protein}g of protein`)
+    const data = new FormData(formEl.current);
+
+    const response = await fetch('http://localhost:8080/recipe', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt'),
+      }),
+    });
+
+    const recipe = await response.json();
+    // resultEl.innerHTML = `<p>${recipe.aiRecipe}</p>`
+    console.log(recipe.aiRecipe)
   }
   
   return (
-    <form onSubmit={handleSubmit}>
+    <form ref={formEl} onSubmit={handleSubmit}>
       <div className="flex flex-col h-screen justify-center items-center">
           <div className="py-5">
             <label className="text-lg font-medium text-gray-100">
@@ -21,8 +37,9 @@ const RecipeGenerator = () => {
             <input
                 className="w-full rounded-md py-2 pl-1 text-gray-100 placeholder:text-gray-100"
                 type="text"
+                name="prompt"
                 value={recipe}
-                maxLength={30}
+                maxLength={100}
                 placeholder="Cookies, Pizza?"
                 onChange={(e) => setRecipe(e.target.value)} />
           </div>
@@ -53,6 +70,7 @@ const RecipeGenerator = () => {
           <div className="py-5">
             <input className="rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit" />
           </div>
+          <p ref={resultEl} className="text-white" id="result">Result will show up here</p>
       </div>
     </form>
   )
