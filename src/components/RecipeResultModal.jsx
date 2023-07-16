de5ab5ec518c0b91f6d9c16fe9dbfce704ebe8df
Sean McDaniel <seanhb714@gmail.com>
2023-07-16 12:23:31 -0700

    adjust result spacing

--- src/components/RecipeResultModal.jsx
+++ src/components/RecipeResultModal.jsx
@@ -3,7 +3,7 @@ import "../styles/recipe-result-modal.css";
 const RecipeResultModal = (props) => {
   const recipeResult = props.recipeResult;
   return (
-    <div className="recipe-result-modal max-w-lg p-5">
+    <div className="recipe-result-modal px-5">
       <p className="recipeResult">{recipeResult}</p>
     </div>
   )
