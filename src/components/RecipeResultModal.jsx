4cb3952a63f497a8751fb32f1ed0bbe9e73e04c8
Sean McDaniel <seanhb714@gmail.com>
2023-08-12 13:01:37 -0700

    Update modal button color

--- src/components/RecipeResultModal.jsx
+++ src/components/RecipeResultModal.jsx
@@ -36,7 +36,7 @@ const RecipeResultModal = (props) => {
             exit="exit"
           >
           <p>{recipeResult}</p>
-          <button className="rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={props.handleClose}>Close</button>
+          <button className="rounded-md bg-yellow-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600" onClick={props.handleClose}>Close</button>
       </motion.div>
     </RecipeResultBackdrop>
   )
