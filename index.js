fb20ece7c08740f1fd56d592b9cf6e384b8a81ee
Sean McDaniel <seanhb714@gmail.com>
2023-08-24 12:49:44 -0700

    updated response directory

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post('/recipe', async (req, res) => {
+app.post('/', async (req, res) => {
 
   const prompt = req.body.prompt
 
@@ -30,6 +30,6 @@ app.post('/recipe', async (req, res) => {
   res.send({ aiRecipe });
 });
 
-app.listen(PORT, () => console.log(`Running on port https://localhost:${PORT}/recipe`));
+app.listen(PORT, () => console.log(`Running on port https://localhost:${PORT}/`));
 
 // export default app;
