02ad9eec537523e0bdb51f36a9c90f4e5ab8c008
Sean McDaniel <seanhb714@gmail.com>
2023-08-24 22:50:20 -0700

    update api

--- src/components/RecipeGenerator.jsx
+++ src/components/RecipeGenerator.jsx
@@ -24,7 +24,7 @@ const RecipeGenerator = () => {
     e.preventDefault();
     const data = new FormData(formEl.current);
 
-    const response = await fetch('https://macros-ai-seanxdon.onrender.com/recipe', {
+    const response = await fetch('https://macros-api-uc2c.onrender.com/api', {
       method: 'POST',
       headers: {
         'Content-Type' : 'application/json',
